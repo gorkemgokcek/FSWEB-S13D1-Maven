b6f919e9ef077d1b3a1ba4919e3ecc3646ead121
Gorkem Gokcek <gorkemgokcekk@gmail.com>
2024-03-18 22:59:29 +0300

    FSWeb-S13-D1-Project completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,40 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        } else return isBarking && (clock < 8 || clock > 22);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
+    }
+
+    private static boolean isTeen(int age) {
+        return (age >= 13 && age <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (isSummer) {
+            return (temp >= 25 && temp <= 45);
+        } else {
+            return (temp >= 25 && temp <= 35);
+        }
     }
 
-    public static double area(double width, double height) {
-       
+    public static double area(double width, double height){
+         if(width< 0||height< 0){
+        return -1;
+    }
+        return width *height;
     }
 
-    public static double area(double radius) {
-       
+    public static double area(double radius){
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
